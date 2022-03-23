1017228b267925b372adef3fb6e9cb9113c1445d
JoakimPekola <joakim.pekola@edu.keuda.fi>
2022-03-23 17:56:33 +0200

    pystyy tallentamaan koko pohja ja kastike

--- Projekti/Tilaus_sivu/js/fantasia.js
+++ Projekti/Tilaus_sivu/js/fantasia.js
@@ -1,17 +1,35 @@
 
 function koko(){       
     var ele = document.getElementsByName('koko');
-    localStorage.setItem("test",ele.length)
     const kokoja = ["small","medium","large"]
     for (i = 0; i <= ele.length; i++) {
         if (ele[i].checked) {
-            localStorage.setItem("innerText", "aaa")
-            selectedSize = ele[i];
             localStorage.setItem("fantasia",kokoja[i])
             break;
         }
     }
+    var ela = document.getElementsByName('pohja');
+    const pohjia = ["norm","gluteeniton"]
+    for (i = 0; i <= ela.length; i++) {
+        if (ela[i].checked) {
+            let a = [localStorage.getItem("fantasia"),pohjia[i]]
+            localStorage.setItem("fantasia",a)
+            break;
+        }
+    }
+    var eli = document.getElementsByName('kastike');
+    const kastikkeita = ["valkosipuli","ilman"]
+    for (i = 0; i <= eli.length; i++) {
+        if (eli[i].checked) {
+            let a = [localStorage.getItem("fantasia"),kastikkeita[i]]
+            localStorage.setItem("fantasia",a)
+            break;
+        }
+    }
 };
+function pohja(){
+    localStorage.setItem("pohja","e")
+}
 function lisaa(a){
     let summa = parseInt(document.getElementById(a).textContent)
     summa += 1   
