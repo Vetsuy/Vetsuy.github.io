c122cefcaab5b5952e7530436d9c0c94054fbca6
JoakimPekola <joakim.pekola@edu.keuda.fi>
2022-03-25 00:09:09 +0200

    Update valmis.js

--- Projekti/Tilaus_sivu/js/valmis.js
+++ Projekti/Tilaus_sivu/js/valmis.js
@@ -1,24 +1,24 @@
 addEventListener("DOMContentLoaded",kuvat)
 function kuvat(){
-    if(localStorage.getItem("kiva")=="juusto"){
+    if(localStorage.getItem("kuva")=="juusto"){
      document.getElementById("kuvaa").src= "uploads/juusto-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="pepperoni"){
+    if(localStorage.getItem("kuva")=="pepperoni"){
      document.getElementById("kuvaa").src= "uploads/pepperoni-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="kinkku"){
+    if(localStorage.getItem("kuva")=="kinkku"){
      document.getElementById("kuvaa").src= "uploads/kinkku-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="tonni"){
+    if(localStorage.getItem("kuva")=="tonni"){
      document.getElementById("kuvaa").src= "uploads/tonnikala-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="kebab"){
+    if(localStorage.getItem("kuva")=="kebab"){
      document.getElementById("kuvaa").src= "uploads/kebab-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="vege"){
+    if(localStorage.getItem("kuva")=="vege"){
      document.getElementById("kuvaa").src= "uploads/vegaani-pizza.jpg"
     }
-    if(localStorage.getItem("kiva")=="jauhe"){
+    if(localStorage.getItem("kuva")=="jauhe"){
      document.getElementById("kuvaa").src= "uploads/jauheliha-pizza.jpg"
     }
 }
